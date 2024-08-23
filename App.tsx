94b74585dcb4b9c857c07fcf54adcaa1800375d6
DinhNguyenChung <dinhvanchung24042003@gmail.com>
2024-08-23 16:42:22 +0700

    usestate and button

--- App.tsx
+++ App.tsx
@@ -1,15 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name,setName] = useState<string>('Đinh Nguyên Chung');
+  const [test, setTest] = useState({
+    name : 'Đinh Nguyên Chung',
+    age: 21,
+    alert: "Hãy cố gắng học thật tốt nhé !",
+  });
+  const [count, setCount] = useState<number>(0);
+
+
   return (
     <View style={styles.container}> 
-      <Text style= {styles.parent}> Hello ! Đinh Nguyên Chung 
+      <Text style= {styles.parent}> Hello ! {name}
           <Text style={styles.child}> Wellcom to Android</Text>
         </Text> 
-      <Text>Hãy cố gắng học thật tốt nhé !</Text>
+      <Text>{test.alert}</Text>
       <StatusBar style="auto" />
+      <View>
+        {/* <Button title='Click me' onPress={() => alert('Đinh Nguyên Chung cố lên !')}/> */}
+        <Button title='Increase' onPress={() => setCount(count + 1)}/>
+        <Text>Count: {count}</Text>
+      </View>
     </View>
+    
   );
 }
 
@@ -28,10 +45,12 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     textAlign: 'center',
+    
   },
   child: {
     color: 'blue',
     fontSize: 20,
+    fontWeight: "600"
 
   }
 });
